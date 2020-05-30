d29fa621055a289999eb2be79181b46dda9a82ed
PickleMad <58864096+PickleMad@users.noreply.github.com>
2020-05-30 14:13:11 +0100

    popup introudcted w/discount cards + imgs applied + more responsiveness added

---
+++ scripts/list-builder.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    var delay = 300; // milliseconds
+    var cookie_expire = 0; // days
+
+    var cookie = localStorage.getItem("list-builder");
+    if (cookie == undefined || cookie == null) {
+        cookie = 0;
+    }
+
+    if (((new Date()).getTime() - cookie) / (1000 * 60 * 60 * 24) > cookie_expire) {
+        $("#list-builder").delay(delay).fadeIn("fast", () => {
+            $("#popup-box").fadeIn("fast", () => { });
+        });
+
+
+        $("#popup-close").click(() => {
+            $("#list-builder, #popup-box").hide();
+            localStorage.setItem("list-builder", (new Date()).getTime());
+        });
+
+        $("#thanks").click(() => {
+            $("#list-builder, #popup-box").hide();
+            localStorage.setItem("list-builder", (new Date()).getTime());
+        });
+
+
+    }
+
+});
